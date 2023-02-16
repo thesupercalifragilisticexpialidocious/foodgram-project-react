f495d1c9e2baf23decdd94856fa1128f511103db
ConstConstantin <cmsteltsov@ya.ru>
2023-02-17 01:01:48 +0300

    models

---
+++ backend/recipes/models.py
@@ -0,0 +1,156 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+from ..users.models import User
+
+
+UNIT_CHOICES = (
+    ('G', 'г'),
+    ('KG', 'кг'),
+    ('ML', 'мл'),
+    ('TSP', 'ч. л.'),
+    ('TBSP', 'ст. л.'),
+    ('PC', 'шт.'),
+    ('AD_LIB', 'по вкусу'),
+    ('GLASS', 'стакан'),
+    ('DROP', 'капля'),
+    ('HANDFUL', 'горсть'),
+    ('SLICE', 'кусок'),
+    ('PACK', 'пакет'),
+    ('BUNCH', 'пучок'),
+    ('PINCH', 'щепотка'),
+    ('CAN', 'банка'),
+    ('PACKAGE', 'упаковка'),
+)
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        unique=True,
+        max_length=32,
+        verbose_name='название'
+    )
+    color = models.PositiveIntegerField(unique=True)
+    # normalize to #49B64E
+    slug = models.SlugField(
+        unique=True,
+        max_length=16,
+        verbose_name='слаг'
+    )
+
+    class Meta:
+        default_related_name = 'tag'
+        verbose_name = 'тэг'
+        verbose_name_plural = 'тэги'
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Ingridient(models.Model):
+    name = models.CharField(
+        unique=True,
+        max_length=64,
+        verbose_name='название'
+    )
+    unit = models.CharField(
+        choices=UNIT_CHOICES,
+        max_length=16,
+        verbose_name='ед. измерения'
+    )
+
+    class Meta:
+        default_related_name = 'ingridient'
+        verbose_name = 'ингридиент'
+        verbose_name_plural = 'ингридиенты'
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Recipe(models.Model):
+    STR_PRESENTATION = ('{title} by {author} ({pub_date:.%Y.%m.%d})')
+    title = models.CharField(
+        max_length=64,
+        verbose_name='название',
+    )
+    description = models.TextField(
+        max_length=4096,
+        unique=True,
+        verbose_name='описание',
+    )
+    duration = models.DurationField(verbose_name='время приготовления')
+    image = models.ImageField(
+        upload_to='recipes/',
+        unique=True,
+        verbose_name='фото',
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='дата и время опубликования',
+    )
+    author = models.ForeignKey(
+        User,
+        null=False,
+        on_delete=models.CASCADE,
+        related_name='recipies',
+    )
+    tag = models.ManyToManyField(
+        Tag,
+        blank=False,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='recipes'
+    )
+
+    class Meta:
+        default_related_name = 'recipe'
+        ordering = ('-pub_date', )
+        verbose_name = 'рецепт'
+        verbose_name_plural = 'рецепты'
+
+    def __str__(self):
+        return self.STR_PRESENTATION.format(
+            title=self.title,
+            author=self.author.username,
+            pub_date=self.pub_date,
+        )
+
+
+class IngridientPerRecipe(models.Model):
+    STR_PRESENTATION = '{recipe} requires {amount} {unit} of {foodstuff}'
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='including',
+    )
+    ingridient = models.ForeignKey(
+        Ingridient,
+        on_delete=models.CASCADE,
+        related_name='used_in',
+    )
+    amount = models.FloatField(
+        validators=[
+            MinValueValidator(0.01, message='Укажите число не менее 0.01'),
+            MaxValueValidator(8192, message='Укажите число до 8000')
+        ],
+        verbose_name='Оценка'
+    )
+
+    class Meta:
+        verbose_name = 'ингридиент на блюдо'
+        verbose_name_plural = 'ингридиенты на блюда'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('recipe', 'ingridient'),
+                name='unique_ingredient_dish_relation'
+            ),
+        )
+
+    def __str__(self):
+        return self.STR_PRESENTATION.format(
+            recipe=self.recipe.title,
+            amount=self.amount,
+            unit=self.ingridient.unit,
+            foodstuff=self.ingridient.name
+        )
---
+++ backend/users/models.py
@@ -0,0 +1,100 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from ..recipes.models import Recipe
+
+User = get_user_model()
+
+
+class Follow(models.Model):
+    STR_PRESENTATION = '@{user} follows @{author}'
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follows',
+        verbose_name='подписчик',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='followed_by',
+        verbose_name='автор',
+    )
+
+    class Meta:
+        verbose_name = 'подписка'
+        verbose_name_plural = 'подписки'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('user', 'author'),
+                name='unique_follow_relation'
+            ),
+        )
+
+    def __str__(self):
+        return self.STR_PRESENTATION.format(
+            user=self.user.username,
+            author=self.author.username,
+        )
+
+
+class Favorite(models.Model):
+    STR_PRESENTATION = '{user} favors {recipe}'
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='favors',
+        verbose_name='добавил в избранное',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='favored_by',
+        verbose_name='избранный рецепт',
+    )
+
+    class Meta:
+        verbose_name = 'избранное'
+        verbose_name_plural = 'избранные'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('user', 'recipe'),
+                name='unique_favorite_relation'
+            ),
+        )
+
+    def __str__(self):
+        return self.STR_PRESENTATION.format(
+            user=self.user.username,
+            recipe=self.recipe.title,
+        )
+
+
+"""
+class User(AbstractUser):
+    ADMIN = 'admin'
+    MODERATOR = 'moderator'
+    USER = 'user'
+
+    ROLES = [
+        (USER, 'User'),
+        (MODERATOR, 'Moderator'),
+        (ADMIN, 'Administrator'),
+    ]
+
+    bio = models.CharField(max_length=250, null=True, blank=True)
+    role = models.CharField(max_length=20, default='user', choices=ROLES)
+    confirmation_code = models.CharField(max_length=10, null=True, blank=True)
+    email = models.EmailField(unique=True)
+
+    @property
+    def is_moderator(self):
+        return self.role == self.MODERATOR
+
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN
+
+    class Meta:
+        ordering = ['id']
+"""
